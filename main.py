2b13bd0ce864487ced95aeee8239b4f9dea78d2f
hasanbykkk <99807200+hasanbykkk@users.noreply.github.com>
2022-11-24 13:40:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+def su_hesapla(kilo):
+    k_hesapla=kilo  *0.03
+    e_hesapla=kilo  *0.04
+    cinsiyet=input("lütfen cinsiyetinizi giriniz").lower()
+    if cinsiyet=="erkek":
+        print("-" * 30)
+        print(e_hesapla,"litre su içmelisiniz")
+        print("cinsiyetiniz", cinsiyet)
+        print("-" * 30)
+    elif cinsiyet=="kadın":
+        print("-" * 30)
+        print("cinsiyetiniz",cinsiyet)
+        print(k_hesapla,"litre su içmelisiniz")
+        print("-" * 30)
+    elif not cinsiyet:
+        print("-"*30)
+        print("lütfen cinsiyetinizi giriniz")
+        print("-"*30)
+kilo_al=int(input("lütfen kilonuzu giriniz"))
+
+su_hesapla(kilo_al)
+def yag_oran(yag):
+
